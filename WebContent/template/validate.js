9a1c13959a487a43d1ea6879f2b4ef333767cb73
Shantnu Agarwal <shantnu.agarwal@gmail.com>
2019-06-24 16:01:03 +0530

    update Validation Service

--- WebContent/template/validate.js
+++ WebContent/template/validate.js
@@ -3,6 +3,8 @@ bootstrapValidate('#validationName',
 		'max:25:Name cannot be longer than 25 characters!');
 bootstrapValidate('#validationName',
 		'min:5:Name must be longer than 5 characters!');
+bootstrapValidate('#validationName',
+'regex:^[a-z ,.\'-]+$:Your name must consist of letters only!');
 bootstrapValidate('#validationEmail', 'email:Enter a valid email address');
 bootstrapValidate('#validationMobileNumber',
 		'numeric:You can only enter numbers for your mobile number!');
@@ -14,6 +16,11 @@ bootstrapValidate('#validationUsername',
 		'max:25:Username cannot be longer than 25 characters!');
 bootstrapValidate('#validationUsername',
 		'min:5:Username must be longer than 5 characters!');
+bootstrapValidate('#validationUsername',
+'alphanum:Your username cannot consist of special characters and white spaces');
 bootstrapValidate(
 		'#validationConfirmPassword',
 		'matches:#validationPassword:Please check that your password matches in both the fields');
+bootstrapValidate(
+		'#validationPassword',
+		'matches:#validationConfirmPassword:Please check that your password matches in both the fields');
